1caaa07c2d114aa7655b7d12719f6c56936e840f
Valentin Parkhomenko <parkhomenko.valentin@megogo.net>
2022-03-10 20:27:37 +0200

    Not working auto-solver (incompatible actions)

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
+from copy import copy
+
+import action as action
+
 from solver import Solver
 from threading import Thread
+
+active_threads = []
+done_moves = 0
+
+
+def move_colors(solver, from_bottle, to_bottle):
+    global done_moves
+    done_moves += 1
+    result = solver.move_water(from_bottle, to_bottle)
+    print('Done moves: ', done_moves)
+    return result
+
+
+def find_solutions_thread(solver):
+    global active_threads
+    solver.check_solved()
+    if solver.solved:
+        print('SOLVED! ', solver.get_map())
+    if len(solver.actions) > 0:
+        for action in solver.actions:
+            result = move_colors(Solver(solver.get_map()), action['water'].bottle.index, action['compatible'].bottle.index)
+            result.find_actions()
+            thread = Thread(target=find_solutions_thread, args=(result,))
+            thread.start()
+            active_threads.append(thread)
+    else:
+        print('Thread stopped! No solutions in', solver.get_map())
+
 
 if __name__ == '__main__':
     print("Welcome to Water Sorting Puzzle Solver developed by Astro1247 [WIP]")  # TODO remove [WIP] tag
     print("Please, input colors or enter empty iput when done")
-    map = [[1,1,1,0],[2,2,2,1],[2,0,0,0]]#[[]]
-    data = ''#input("Enter color (or just press ENTER):")
+    map = [[1, 1, 1, 0], [2, 2, 2, 1], [2, 0, 0, 0], [0, 0, 0, 0]]  # [[]]
+    data = ''  # input("Enter color (or just press ENTER):")
     while data != '':
         try:
             data = int(data)
@@ -20,4 +52,7 @@ if __name__ == '__main__':
             print("Invalid input, please try again")
         data = input("Enter color (or just press ENTER):")
     slv = Solver(map)
-    print('Got:', slv)
+    find_solutions_thread(slv)
+    for thread in active_threads:
+        thread.join()
+    print('Job finished')
--- solver.py
+++ solver.py
@@ -1,38 +1,47 @@
 class Water(object):
-    color = None
-    size = 1
-    bottle_position = {
-        'from': None,
-        'to': None
-    }
-
-    def __init__(self, color, size, pos_from):
+    def __init__(self, color, size, pos_from, bottle):
         if not isinstance(color, int):
             raise TypeError("Color must be an integer")
         elif not isinstance(size, int):
             raise TypeError("Size must be an integer")
-        elif size not in range(1,5):
+        elif size not in range(1, 5):
             raise ValueError("Size must be in range 1-4, inclusive")
-        elif pos_from not in range(1,5):
+        elif pos_from not in range(1, 5):
             raise ValueError("Pos_from must be in range 1-4, inclusive")
+        elif not isinstance(bottle, Bottle):
+            raise TypeError('Bottle must be Bottle class instance')
         self.size = size
         self.color = color
-        self.bottle_position['from'] = pos_from
-        self.bottle_position['to'] = pos_from+size-1
+        self.bottle_position = {
+            'from': pos_from,
+            'to': pos_from + size - 1
+        }
+        self.bottle = bottle
+        if self.bottle_position['to'] > 4:
+            raise OverflowError("Bottle position must be in range 1-4")
+
+    def increase(self, count):
+        if self.bottle_position['to'] == 4:
+            raise OverflowError("Water is already at bottle top")
+        elif self.bottle_position['to'] + count > 4 or self.size + count > 4:
+            raise OverflowError("Water position/size cannot overlap 4")
+        else:
+            self.size += count
+            self.bottle_position['to'] = self.bottle_position['from'] + self.size - 1
+        return self.size
+
 
 class Bottle(object):
-    data = []
-    solved = False
-    index = None
-
-    def __init__(self, data):
+    def __init__(self, data, solver):
         if not isinstance(data, list):
             raise TypeError("data must be a list")
         elif len(data) != 4:
             raise ValueError("data must have 4 values")
         elif not all(isinstance(elem, int) for elem in data):
             raise ValueError("data must contain elements of type int")
-        #self.data = list(filter(lambda x: x != 0, data))
+        self.data = []
+        self.solved = False
+        self.index = len(solver.bottles)
         data = list(filter(lambda x: x != 0, data))
         groupped_data = []
         for elem in data:
@@ -44,7 +53,7 @@ class Bottle(object):
                 else:
                     groupped_data.append({'color': elem, 'size': 1})
         for elem in groupped_data:
-            self.data.append(Water(elem['color'], elem['size'], self.get_fullness()+1))
+            self.data.append(Water(elem['color'], elem['size'], self.get_fullness() + 1, self))
         self.check_solved()
 
     def check_solved(self):
@@ -62,7 +71,7 @@ class Bottle(object):
         if self.get_fullness() > 0:
             return self.data[-1]
         else:
-            return None
+            return Water(0, 1, 1, self)
 
     def pull_top_color(self):
         if self.solved:
@@ -81,14 +90,22 @@ class Bottle(object):
             self.data.append(color)
             self.check_solved()
 
+    def get_mapped_data(self):
+        mapped = []
+        if len(self.data) == 0:
+            return [0,0,0,0]
+        for color in self.data:
+            for i in range(0,color.size):
+                mapped.append(color.color)
+        for i in range(len(mapped),4):
+            mapped.append(0)
+        return mapped
+
 
 class Solver(object):
-    bottles = []
-    solved = False
-    finished = False
-
     def __init__(self, map):
-        # validating map Expected to be like [[1,1,2,1], [2,2,1,2], [3,5,3,9], [0,0,0,0]], starting from bottle bottom
+        # validating map Expected to be like [[1,1,2,1], [2,2,1,2], [3,5,3,0], [0,0,0,0]], starting from bottle bottom
+        # where 0 = empty
         if not isinstance(map, list):
             raise TypeError("map must be a list")
         elif len(map) < 1:
@@ -100,16 +117,70 @@ class Solver(object):
                 raise TypeError("Bottle elements must be integers")
             elif any(len(bottle) != 4 for bottle in map):
                 raise TypeError("Bottle must contain 4 elements")
+        self.map = map
+        self.bottles = []
+        self.solved = False
         for data in map:
-            self.bottles.append(Bottle(data))
-        #self.bottles = [Bottle(data) for data in map]
+            x = Bottle(data, self)
+            self.bottles.append(x)
         if not self.check_solved():
-            self.find_actions()
+            self.actions = self.find_actions()
+        else:
+            self.actions = []
 
     def check_solved(self):
         self.solved = all(bottle.solved for bottle in self.bottles)
         return self.solved
 
     def find_actions(self):
+        self.check_solved()
+        if self.solved:
+            return []
         bottles_tops = [bottle.get_top_data() for bottle in self.bottles]
-        return bottles_tops
+        possible_actions = []
+        for top in bottles_tops:
+            if top.color == 0:
+                continue
+            compatibles = list(filter(
+                lambda target: (target.color == 0 or target.color == top.color) and target.bottle_position['to'] + top.size <= 4,
+                bottles_tops))
+            if len(compatibles) > 0:
+                for compatible in compatibles:
+                    if compatible.bottle != top.bottle:
+                        possible_actions.append({'compatible': compatible, 'water': top})
+        self.actions = possible_actions
+        [print('Possible action found. {} ({}) bottle {} into {} ({}) bottle {}'.format(action['water'].color,
+                                                                                        action['water'].size,
+                                                                                        action['water'].bottle,
+                                                                                        action['compatible'].color,
+                                                                                        action['compatible'].size,
+                                                                                        action['compatible'].bottle))
+         for action in self.actions]
+        return possible_actions
+
+    def move_water(self, from_bottle, to_bottle):
+        #from_bottle = source_bottle.index
+        #to_bottle = target_bottle.index
+        source_bottle = self.bottles[from_bottle]
+        target_bottle = self.bottles[to_bottle]
+        if from_bottle not in range(0,len(self.bottles)):
+            raise IndexError('Index "from_bottle" is out of range')
+        elif to_bottle not in range(0,len(self.bottles)):
+            raise IndexError('Index "to_bottle" is out of range')
+        elif source_bottle.get_top_data().color == 0:
+            raise ValueError('Cannot move water from empty bottle')
+        elif source_bottle.get_top_data().color != target_bottle.get_top_data().color \
+                and target_bottle.get_top_data().color != 0:
+            raise ValueError(f'Colors in bottles are incompatible {source_bottle.get_top_data().color} into {target_bottle.get_top_data().color}')
+        elif source_bottle.get_top_data().size + target_bottle.get_top_data().bottle_position['to'] > 4:
+            raise OverflowError("Target bottle will be overflowed")
+        target_water = target_bottle.get_top_data()
+        if target_water.color == 0:
+            target_bottle.push_color(source_bottle.pull_top_color())
+        else:
+            target_water.increase(source_bottle.pull_top_color().size)
+        return self
+
+    def get_map(self):
+        self.map = [bottle.get_mapped_data() for bottle in self.bottles]
+        return self.map
